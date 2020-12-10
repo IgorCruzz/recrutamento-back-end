5c5dc7b405c3b68d679ac512673d6d05cc04f471
igorcruzz <igorskt2009@gmail.com>
2020-12-10 14:46:18 -0300

    feat: createUser should return undefined not find any user with email passed

--- src/infra/database/repositories/User.repository.ts
+++ src/infra/database/repositories/User.repository.ts
@@ -1,3 +1,4 @@
+import { IFindUserByEmail } from '@/data/protocols/database/user/FindUserByEmail.interface'
 import {
   ICreateUser,
   IUserModel,
@@ -5,11 +6,17 @@ import {
 import { getRepository } from 'typeorm'
 import { User } from '../entities/User.entity'
 
-export class UserRepository implements ICreateUser {
+export class UserRepository implements ICreateUser, IFindUserByEmail {
   private readonly UserRepository = getRepository(User)
 
   async create(email: string): Promise<IUserModel> {
     return await this.UserRepository.save({
+      email,
+    })
+  }
+
+  async findMail(email: string): Promise<IUserModel> {
+    return await this.UserRepository.findOne({
       email,
     })
   }
