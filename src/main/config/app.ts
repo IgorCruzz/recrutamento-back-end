5eb8b7c6abb4414fbedf9860ed834157324a9f5a
igorcruzz <igorskt2009@gmail.com>
2020-12-10 12:48:51 -0300

    refactor: change circle config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,1 +1,14 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import { serve, setup } from 'swagger-ui-express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  return res.json({ ok: 'ok' });
+});
+
+export default app;
