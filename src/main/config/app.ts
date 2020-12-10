752d6e02f9bd9d8a33d1b63516aa049c8ecc66be
igorcruzz <igorskt2009@gmail.com>
2020-12-10 17:17:13 -0300

    chore: add cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,11 @@
 import 'reflect-metadata'
-import express, { Request, Response, NextFunction } from 'express'
+import express, { Request, Response, NextFunction, json } from 'express'
 import cors from 'cors'
 import { serve, setup } from 'swagger-ui-express'
 
 const app = express()
 
-app.use(express.json())
+app.use(json())
+app.use(cors())
 
 export default app
