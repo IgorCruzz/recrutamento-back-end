87d4ff444717c31f45fcb2a9c685fa740fc8d3fe
igorcruzz <igorskt2009@gmail.com>
2020-12-10 15:36:00 -0300

    feat: ensure DbCreateUser should be call findUserByEmail with success

--- src/data/usecases/user/DbCreateUser.data.ts
+++ src/data/usecases/user/DbCreateUser.data.ts
@@ -1,12 +1,15 @@
+import { IFindUserByEmail } from '@/data/protocols/database/user/FindUserByEmail.interface'
 import {
   ICreateUser,
   ICreateUserResult,
 } from '@/domain/usecases/user/CreateUser.domain'
 
 export class DbCreateUser implements ICreateUser {
-  constructor() {}
+  constructor(private readonly findUserByEmail: IFindUserByEmail) {}
 
   async createUser(email: string): Promise<ICreateUserResult> {
+    const findUser = await this.findUserByEmail.findMail(email)
+
     return await null
   }
 }
