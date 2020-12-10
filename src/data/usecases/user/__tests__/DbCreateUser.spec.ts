b6456c8ecad290d1c8a8f0000459b119e2f1d004
igorcruzz <igorskt2009@gmail.com>
2020-12-10 15:35:28 -0300

    test: ensure DbCreateUser should be call findUserByEmail with success

--- src/data/usecases/user/__tests__/DbCreateUser.spec.ts
+++ src/data/usecases/user/__tests__/DbCreateUser.spec.ts
@@ -1,14 +1,26 @@
+import { FindUserByEmailStub } from '@/data/mocks/userRepo.mock'
+import { IFindUserByEmail } from '@/data/protocols/database/user/FindUserByEmail.interface'
 import { ICreateUser } from '@/domain/usecases/user/CreateUser.domain'
 import { DbCreateUser } from '../DbCreateUser.data'
 
 let dBCreateUser: ICreateUser
+let findUserByEmail: IFindUserByEmail
 
 describe('DbCreateUser  ( DATA )', () => {
   beforeEach(() => {
-    dBCreateUser = new DbCreateUser()
+    findUserByEmail = new FindUserByEmailStub()
+    dBCreateUser = new DbCreateUser(findUserByEmail)
   })
 
   it('should be defined', () => {
     expect(dBCreateUser).toBeDefined()
+  })
+
+  it('should be call findUserByEmail with success', async () => {
+    const res = jest.spyOn(findUserByEmail, 'findMail')
+
+    await dBCreateUser.createUser('user@mail.com')
+
+    expect(res).toHaveBeenCalledWith('user@mail.com')
   })
 })
