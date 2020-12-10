f7652183f8ce12e12afcdbc831dade14be4c0a28
igorcruzz <igorskt2009@gmail.com>
2020-12-10 15:17:55 -0300

    feat: create user repo mock

---
+++ src/data/mocks/userRepo.mock.ts
@@ -0,0 +1,29 @@
+import {
+  ICreateUser,
+  IUserModel,
+} from '../protocols/database/user/CreateUser.interface'
+import { IFindUserByEmail } from '../protocols/database/user/FindUserByEmail.interface'
+
+export class CreateUserStub implements ICreateUser {
+  async create(email: string): Promise<IUserModel> {
+    return Promise.resolve({
+      id: 1,
+      email: 'user@mail.com',
+      password_hash: undefined,
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
+  }
+}
+
+export class FindUserByEmailStub implements IFindUserByEmail {
+  async findMail(email: string): Promise<IUserModel> {
+    return Promise.resolve({
+      id: 1,
+      email: 'user@mail.com',
+      password_hash: null,
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
+  }
+}
