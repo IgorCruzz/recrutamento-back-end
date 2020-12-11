1e1b08991d849d5466019f9fe8b38b72a8347a68
igorcruzz <igorskt2009@gmail.com>
2020-12-11 12:57:38 -0300

    test: signin should able to signin

---
+++ src/main/routes/signin/__tests__/Signin.test.ts
@@ -0,0 +1,48 @@
+import app from '../../../config/app'
+import request from 'supertest'
+import { getRepository } from 'typeorm'
+import { connection } from '@/main/config/connection'
+import { User } from '@/infra/database/entities/User.entity'
+import { hash } from 'bcryptjs'
+
+jest.setTimeout(30000)
+
+describe('User', () => {
+  beforeAll(async () => {
+    await connection.create()
+  })
+
+  afterAll(async () => {
+    await connection.close()
+  })
+
+  beforeEach(async () => {
+    await getRepository(User).query(`DELETE FROM users`)
+  })
+
+  afterEach(async () => {
+    await getRepository(User).query(`DELETE FROM users`)
+  })
+
+  describe('Signin', () => {
+    it('/POST -> should able to signin', async () => {
+      const passwordHashed = await hash('password', 12)
+
+      const user = await getRepository(User).save({
+        email: 'user@mail.com',
+      })
+
+      await getRepository(User).update(user.id, {
+        password_hash: passwordHashed,
+      })
+
+      const res = await request(app)
+        .post('/api/signin')
+        .send({
+          email: 'user@mail.com',
+          password: 'password',
+        })
+        .expect(201)
+    })
+  })
+})
