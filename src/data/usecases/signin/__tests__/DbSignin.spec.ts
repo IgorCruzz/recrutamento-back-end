b4bf8848528e9c057c95361afeda0a4b863bbff6
igorcruzz <igorskt2009@gmail.com>
2020-12-11 00:02:46 -0300

    test: dbsignin should call bcryptCompare with success

--- src/data/usecases/signin/__tests__/DbSignin.spec.ts
+++ src/data/usecases/signin/__tests__/DbSignin.spec.ts
@@ -1,3 +1,4 @@
+import { BcryptCompareStub } from '@/data/mocks/Bcrypt.mock'
 import { FindUserByEmailRepositoryStub } from '@/data/mocks/userRepo.mock'
 import { ICompare } from '@/data/protocols/bcryptAdapter/Compare.interface'
 import { IFindUserByEmailRepository } from '@/data/protocols/database/user/FindUserByEmail.interface'
@@ -7,13 +8,14 @@ import { DbSignIn } from '../DbSignin.data'
 
 let dbSignIn: ISignIn
 let findUserByEmailRepository: IFindUserByEmailRepository
-let compare: ICompare
+let bcryptCompare: ICompare
 let sign: ISign
 
 describe('DbSignin ( DATA )', () => {
   beforeEach(() => {
     findUserByEmailRepository = new FindUserByEmailRepositoryStub()
-    dbSignIn = new DbSignIn(findUserByEmailRepository)
+    bcryptCompare = new BcryptCompareStub()
+    dbSignIn = new DbSignIn(findUserByEmailRepository, bcryptCompare)
   })
 
   it('should be defined', () => {
@@ -42,5 +44,36 @@ describe('DbSignin ( DATA )', () => {
     })
 
     expect(res).toEqual({ error: 'Não existe um usuário com este e-mail.' })
+  })
+
+  it('should return an error message if findUserByEmailRepository returns an user with password with null value', async () => {
+    const res = await dbSignIn.signIn({
+      email: 'user@mail.com',
+      password: 'password',
+    })
+
+    expect(res).toEqual({
+      error:
+        'Primeiramente, crie uma senha pelo link que enviamos para seu e-mail, no momento do cadastro.',
+    })
+  })
+
+  it('should call bcryptCompare with success', async () => {
+    jest.spyOn(findUserByEmailRepository, 'findMail').mockResolvedValue({
+      id: 1,
+      email: 'user@mail.com',
+      password_hash: 'password',
+      created_at: new Date(),
+      updated_at: new Date(),
+    })
+
+    const res = jest.spyOn(bcryptCompare, 'compare')
+
+    await dbSignIn.signIn({
+      email: 'user@mail.com',
+      password: 'password',
+    })
+
+    expect(res).toHaveBeenCalledWith('password', 'password')
   })
 })
