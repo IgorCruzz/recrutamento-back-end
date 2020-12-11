fee42030f9d876f87a6900b2132498c80aaabbed
igorcruzz <igorskt2009@gmail.com>
2020-12-11 14:10:12 -0300

    test: ensure createresetpassword should call resetPassword with success

--- src/data/usecases/resetPassword/__tests__/DbCreateResetPassword.spec.ts
+++ src/data/usecases/resetPassword/__tests__/DbCreateResetPassword.spec.ts
@@ -1,21 +1,26 @@
 import { CreateResetPasswordRepositoryStub } from '@/data/mocks/ResetPassword.mock'
+import { ResetPasswordStub } from '@/data/mocks/sendgrid.mock'
 import { FindUserByEmailRepositoryStub } from '@/data/mocks/userRepo.mock'
 import { ICreateResetPasswordRepository } from '@/data/protocols/database/resetPassword/CreateUser.interface'
 import { IFindUserByEmailRepository } from '@/data/protocols/database/user/FindUserByEmail.interface'
+import { IResetPassword } from '@/data/protocols/sendGridAdapter/ResetPasswordMail.interface'
 import { ICreateResetPassword } from '@/domain/usecases/resetPassword/CreatePassword.domain'
 import { DbCreateResetPassword } from '../DbCreateResetPassword.data'
 
 let dbCreateResetPassword: ICreateResetPassword
 let findUserByEmailRepository: IFindUserByEmailRepository
 let createResetPasswordRepository: ICreateResetPasswordRepository
+let resetPassword: IResetPassword
 
 describe('CreateResetPassword ( DATA )', () => {
   beforeEach(() => {
     findUserByEmailRepository = new FindUserByEmailRepositoryStub()
     createResetPasswordRepository = new CreateResetPasswordRepositoryStub()
+    resetPassword = new ResetPasswordStub()
     dbCreateResetPassword = new DbCreateResetPassword(
       findUserByEmailRepository,
-      createResetPasswordRepository
+      createResetPasswordRepository,
+      resetPassword
     )
   })
 
@@ -58,5 +63,24 @@ describe('CreateResetPassword ( DATA )', () => {
     await dbCreateResetPassword.createResetPassword('user@mail.com')
 
     expect(res).toHaveBeenCalledWith(1)
+  })
+
+  test('should throw if createResetPasswordRepository throws', async () => {
+    jest
+      .spyOn(createResetPasswordRepository, 'createResetPassword')
+      .mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+    const promise = dbCreateResetPassword.createResetPassword('user@mail.com')
+    await expect(promise).rejects.toThrow()
+  })
+
+  it('should call resetPassword with success', async () => {
+    const res = jest.spyOn(resetPassword, 'resetPassword')
+
+    await dbCreateResetPassword.createResetPassword('user@mail.com')
+
+    expect(res).toHaveBeenCalledWith({ email: 'user@mail.com', token: 'token' })
   })
 })
