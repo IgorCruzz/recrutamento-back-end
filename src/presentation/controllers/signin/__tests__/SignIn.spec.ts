1b5b9c8ddb03da3cbe66f43f9ff22fabd7cc38a1
igorcruzz <igorskt2009@gmail.com>
2020-12-11 00:33:32 -0300

    test: ensure signincontroller  should return 201 if signInController return an session data

--- src/presentation/controllers/signin/__tests__/SignIn.spec.ts
+++ src/presentation/controllers/signin/__tests__/SignIn.spec.ts
@@ -1,6 +1,7 @@
 import { ISignIn } from '@/domain/usecases/signin/signin.domain'
+import { created } from '../../../../presentation/http/http-helper'
 import { DbSignInStub } from '@/presentation/mocks/SignIn.mock'
-import { IController } from '@/presentation/protocols'
+import { IController, IHttpRequest } from '@/presentation/protocols'
 import { SignInController } from '../SignIn.controller'
 
 let signInController: IController
@@ -9,10 +10,25 @@ let signIn: ISignIn
 describe('SignIn ( Controller )', () => {
   beforeEach(() => {
     signIn = new DbSignInStub()
-    signInController = new SignInController()
+    signInController = new SignInController(signIn)
   })
 
   it('should be defined', () => {
     expect(signInController).toBeDefined()
+  })
+
+  it('should return 201 if signInController return an session data', async () => {
+    const req: IHttpRequest = {
+      body: {
+        email: 'user@mail.com',
+        password: 'password',
+      },
+    }
+
+    const res = await signInController.handle(req)
+
+    expect(res).toEqual(
+      created({ id: 1, email: 'user@mail.com', token: 'token' })
+    )
   })
 })
