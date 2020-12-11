3dc508556d148d5902d0c182ed7a42145d187f8c
igorcruzz <igorskt2009@gmail.com>
2020-12-11 00:03:30 -0300

    feat: dbsignin should call bcryptCompare with success

--- requirements/signup.md
+++ requirements/signup.md
@@ -1,7 +1,7 @@
 Caso de sucesso ✅
 
-1.  Validar se existe um usuário com o e-mail passado.
-1.  Verificar se a senha passada tem o valor NULL.
+1.  ✅ Validar se existe um usuário com o e-mail passado.
+1.  ✅ Verificar se a senha do usuário com o e-mail passado tem o valor NULL.
 1.  Verificar se a senha passada está correta.
 1.  Criar uma código criptografado de autenticação.
 1.  Criar uma rota /POST para criar uma sessão
@@ -9,8 +9,8 @@ Caso de sucesso ✅
 
 Exceções
 
-1.  Retorna 400 se não existir um usuário com o e-mail fornecido.
-1.  Retorna 400 se a senha tiver valor NULL.
+1.  ✅ Retorna 400 se não existir um usuário com o e-mail fornecido.
+1.  ✅ Retorna 400 se a senha tiver valor NULL.
 1.  Retorna 400 se a senha tiver errada.
 1.  Retorna 401 se os campos tiverem sido preenchido.
 1.  Retorna 500 se der erro ao tentar criar uma sessão.
--- src/data/usecases/signin/DbSignin.data.ts
+++ src/data/usecases/signin/DbSignin.data.ts
@@ -1,3 +1,4 @@
+import { ICompare } from '@/data/protocols/bcryptAdapter/Compare.interface'
 import { IFindUserByEmailRepository } from '@/data/protocols/database/user/FindUserByEmail.interface'
 import {
   ISignIn,
@@ -7,15 +8,29 @@ import {
 
 export class DbSignIn implements ISignIn {
   constructor(
-    private readonly findUserByEmailRepository: IFindUserByEmailRepository
+    private readonly findUserByEmailRepository: IFindUserByEmailRepository,
+    private readonly bcryptCompare: ICompare
   ) {}
 
   async signIn(data: ISignInDTO): Promise<ISignInDTOResult> {
-    const { email } = data
+    const { email, password } = data
 
     const findUser = await this.findUserByEmailRepository.findMail(email)
 
     if (!findUser) return { error: 'Não existe um usuário com este e-mail.' }
+
+    if (!findUser.password_hash) {
+      return {
+        error:
+          'Primeiramente, crie uma senha pelo link que enviamos para seu e-mail, no momento do cadastro.',
+      }
+    }
+
+    const checkPassword = await this.bcryptCompare.compare(
+      password,
+      findUser.password_hash
+    )
+
     return await null
   }
 }
