e16d2a58bc0110c555c2c16f9bff367ba94c81c1
igorcruzz <igorskt2009@gmail.com>
2020-12-11 23:53:05 -0300

    feat: add port to using on production

--- src/main/Server.ts
+++ src/main/Server.ts
@@ -3,6 +3,6 @@ import { connection } from '../main/config/Connection'
 
 connection.create()
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('Server is running')
 })
